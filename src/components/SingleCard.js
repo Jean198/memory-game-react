fb9853137a25572e51a74c459b97d0e4b36e6a70
Jean198 <jeandedieu.niyigaba@gmail.com>
2022-09-29 21:31:34 +0300

    added card component

---
+++ src/components/SingleCard.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./SingleCard.css";
+
+const SingleCard = ({ card, handleChoice, flipped }) => {
+  const handleClick = () => {
+    handleChoice(card);
+  };
+  return (
+    <div className="card">
+      <div className={flipped ? "flipped" : ""}>
+        <img src={card.src} className="front" alt="card-front" />
+        <img
+          src="/img/cover.png"
+          className="back"
+          alt="card-back"
+          onClick={handleClick}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SingleCard;
